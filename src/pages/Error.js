0313d44c65ced3f24cc5ebb99e3e04a27dbb14d3
AbdelwahabTop <mramged936@gmail.com>
2023-01-07 10:30:07 +0200

    build info component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -7,11 +7,14 @@ const Error = () => {
       <div>
         <h1>404</h1>
         <h3>sorry, the page you tried cannot be found</h3>
-        <Link to='/' className='btn'>back home</Link>
+        <Link to="/" className="btn">
+          back home
+        </Link>
       </div>
     </Wrapper>
   );
 };
+
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
