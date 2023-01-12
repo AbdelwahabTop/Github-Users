484a2e7662960f038e8a598f50c9745a7d5fb91f
AbdelwahabTop <mramged936@gmail.com>
2023-01-12 17:27:53 +0200

    fetching remaining requests from github api

--- src/context/context.js
+++ src/context/context.js
@@ -12,9 +12,28 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [followers, setFollowers] = useState(mockFollowers);
   const [repos, setRepos] = useState(mockRepos);
+  // request loading
+  const [requests, setRequest] = useState(0);
+  const [loading, setLoading] = useState(false);
+  // check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequest(remaining);
+        if (remaining === 0) {
+          // throw error
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  // error
+  useEffect(checkRequests, []);
 
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
       {children}
     </GithubContext.Provider>
   );
