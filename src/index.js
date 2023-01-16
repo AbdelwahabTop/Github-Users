44a96fa79d0748ebdb0793c7d41ce9bf5ce03500
AbdelwahabTop <mramged936@gmail.com>
2023-01-16 22:50:45 +0200

    Privet Route

--- src/index.js
+++ src/index.js
@@ -5,12 +5,22 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { GithubProvider } from "./context/context";
 import { Auth0Provider } from "@auth0/auth0-react";
+//Domain: dev-77mp5esmq5nr368k.us.auth0.com
+// Cleint ID: zzEh9RC9izjaMZiSO18yt9H0W84DFIrS
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-77mp5esmq5nr368k.us.auth0.com"
+      clientId="zzEh9RC9izjaMZiSO18yt9H0W84DFIrS"
+      redirectUri={window.location.origin}
+      cacheLocation="localstorage"
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
