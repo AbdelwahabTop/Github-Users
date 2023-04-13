e12df56340ad439cb9480a7218a7529b95bb736d
AbdelwahabTop <mramged936@gmail.com>
2023-04-13 08:33:51 +0200

    Update Charts

--- src/pages/AuthWrapper.js
+++ src/pages/AuthWrapper.js
@@ -2,6 +2,7 @@ import React, { Children } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import loadingGif from "../images/preloader.gif";
 import styled from "styled-components";
+
 function AuthWrapper({ children }) {
   const { isLoading, error, isAuthenticated, user } = useAuth0();
   const isUser = isAuthenticated && user;
